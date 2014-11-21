9c47e6e3118154f10ddafcf1f67032283ba91db4
John Petersson <johnpetersson@gmail.com>
2014-11-21 19:01:04 +0100

    Initial Commit

---
+++ SolarSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SolarSystem
+//
+//  Created by John Petersson on 2014-11-21.
+//  Copyright (c) 2014 John Petersson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
